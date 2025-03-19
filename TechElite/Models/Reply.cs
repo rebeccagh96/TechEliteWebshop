eef10b7f7779a93d6183a1a4b2d5aaafa5f1b8c1
Rebeccagh <rebecca.gren.hornfeldt.webd24jon@edu.tucsweden.se>
2025-03-19 10:54:30 +0100

    Reply

---
+++ TechElite/Models/Reply.cs
@@ -0,0 +1,13 @@
+﻿namespace TechElite.Models
+{
+    public class Reply
+    {
+        public string ReplyId { get; set; }
+        public string ThreadId { get; set; }
+        public string MemberId { get; set; }
+        public DateTime ReplyDate { get; set; }
+        public string Heading { get; set; }
+        public string ReplyText { get; set; }
+        public string FCategoryId { get; set; }
+    }
+}
