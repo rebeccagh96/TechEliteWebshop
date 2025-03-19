f5e9cfbfcbeeb46448bbc90bbf63483e4498424c
Rebeccagh <rebecca.gren.hornfeldt.webd24jon@edu.tucsweden.se>
2025-03-19 11:14:36 +0100

    ProductDepartment

---
+++ TechElite/Models/ProductDepartment.cs
@@ -0,0 +1,8 @@
+﻿namespace TechElite.Models
+{
+    public class ProductDepartment
+    {
+        public string DepartmentId { get; set; }
+        public string Department {  get; set; }
+    }
+}
