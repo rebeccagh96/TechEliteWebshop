87d5ccddbaccc731b4912277c0a82e4610b74fe5
James Z <Jamie@DESKTOP-MH8BT00>
2025-03-19 14:15:18 +0100

    added js and content

---
+++ TechElite/wwwroot/js/controlTabs.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll(".tabs input[type='radio']");
+    const contentSections = document.querySelectorAll(".content .prof-tab");
+
+    function showContent(selectedTab) {
+        contentSections.forEach(section => section.style.display = "none");
+
+        const tabMapping = {
+            "tab1": "first",
+            "tab2": "konto",
+            "tab3": "aktivitet",
+            "tab4": "inställningar",
+            "tab5": "orderhistorik",
+            "tab6": "recensioner",
+            "tab7": "recensioner"
+        };
+
+        const selectedSection = document.querySelector(`.content .${tabMapping[selectedTab.id]}`);
+        if (selectedSection) {
+            selectedSection.style.display = "block";
+        }
+    }
+
+    tabs.forEach(tab => {
+        tab.addEventListener("change", function () {
+            showContent(this);
+        });
+    });
+
+    const checkedTab = document.querySelector(".tabs input[type='radio']:checked");
+    if (checkedTab) {
+        showContent(checkedTab);
+    }
+});
