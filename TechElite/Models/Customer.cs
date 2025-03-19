2616e97c38d8e882753513196548cf0d6fdc10fa
Alexe <Alexe@ALEX-LAPTOP>
2025-03-19 11:30:52 +0100

    Add Public string till Customer.cs

--- TechElite/Models/Customer.cs
+++ TechElite/Models/Customer.cs
@@ -2,6 +2,16 @@
 {
     public class Customer
     {
+        public string   OrderID { get; set; }
 
+        public string UserID { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Address { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string City { get; set; }
     }
 }
