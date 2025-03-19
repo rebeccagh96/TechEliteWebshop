849525ffffbc8a126b2af149277b8b8180ba52de
Rebeccagh <rebecca.gren.hornfeldt.webd24jon@edu.tucsweden.se>
2025-03-19 10:23:23 +0100

    Thread

---
+++ TechElite/Models/Thread.cs
@@ -0,0 +1,11 @@
+﻿namespace TechElite.Models
+{
+    public class Thread
+    {
+        public string ThreadId { get; set; }
+        public string FCategoryId { get; set; }
+        public string MemberId { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string ThreadReplyText { get; set; }
+    }
+}
