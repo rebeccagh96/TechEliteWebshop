897a259a66e06422f8900336560ff62830311c53
James Z <Jamie@DESKTOP-MH8BT00>
2025-03-19 11:27:02 +0100

    User table

---
+++ TechElite/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TechElite.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string Role { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
