16e41f276cdac7a28b5cf207333fdd9443218c30
James Z <Jamie@DESKTOP-MH8BT00>
2025-03-26 14:22:15 +0100

    Fixed login link

---
+++ TechElite/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+using TechElite.Data;
+using TechElite.Models;
+
+namespace TechElite.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
