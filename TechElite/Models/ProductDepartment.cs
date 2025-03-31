bad5172b2d350ee5ec227d3c6db7cdcaa575727d
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2025-03-31 11:48:35 +0200

    created image array

--- TechElite/Models/ProductDepartment.cs
+++ TechElite/Models/ProductDepartment.cs
@@ -3,6 +3,7 @@
     public class ProductDepartment
     {
         public int ProductDepartmentId { get; set; }
+        public string DepartmentName { get; set; } = default!;
         public string? Department {  get; set; }
         public ICollection<Product>? Products { get; set; }
 
