e5eea2736fbb8c8325efeea54c4c43191c88dd99
Rebeccagh <rebecca.gren.hornfeldt.webd24jon@edu.tucsweden.se>
2025-04-06 15:51:48 +0200

    Added CRUD-operations for threads and some for replys. Added views for forum.

--- TechElite/Controllers/ForumController.cs
+++ TechElite/Controllers/ForumController.cs
@@ -32,61 +32,36 @@ namespace TechElite.Controllers
             return View(model);
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> AddThread(ForumThread forumThread)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var userId = _userManager.GetUserId(User);
-        //        forumThread.UserId = userId;
-        //        if (userId == null)
-        //        {
-        //            return View();
-        //        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _context.ForumCategories.ToListAsync();
+            if (categories == null || !categories.Any())
+            {
+                Console.WriteLine("Inga kategorier hittades.");
+            }
+            ViewBag.Categories = categories;
+            return View();
+        }
 
-        //        var publishDate = DateTime.Now;
-        //        forumThread.PublishDate = publishDate;
-
-        //        _context.Add(forumThread);
-        //        await _context.SaveChangesAsync();
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    else
-        //    {
-        //        return View(); 
-        //    }
-
-        //}
-
-        // Ej testat men kanske fungerar att göra så här istället? / J
-
-        //[HttpPost]
-        //public async Task<IActionResult> AddThread(ForumThread forumThread)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var userName = _userManager.GetUserName(User);
-        //        forumThread.UserName = userName;
-        //        if (userName == null)
-        //        {
-        //            return View();
-        //        }
-
-        //        var publishDate = DateTime.Now;
-        //        forumThread.PublishDate = publishDate;
-
-        //        _context.Add(forumThread);
-        //        await _context.SaveChangesAsync();
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    else
-        //    {
-        //        return View();
-        //    }
-
-        //}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int categoryId, string threadTitle, string threadContent)
+        {
+            List<ForumThread> forumThreads = await _context.ForumThreads.ToListAsync();
+            var forumThread = new ForumThread
+            {
+                CategoryId = categoryId,
+                ThreadTitle = threadTitle,
+                ThreadContent = threadContent,
+                ApplicationUserId = "USER1-STATIC-ID",
+                UserName = "user1",
+                PublishDate = DateTime.Now
+            };
+            _context.ForumThreads.Add(forumThread);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
         public IActionResult LatestThreads()
         {
@@ -98,64 +73,111 @@ namespace TechElite.Controllers
             return View(model);
         }
 
-        /*[HttpGet]
-        public async Task<IActionResult> ShowLatestThreads()
-        {
-            var forumThreads = await _context.ForumThreads.ToListAsync();
-            return View(forumThreads);
-        }*/
-
-        /*[HttpGet]
-        public IActionResult LatestThreads()
-        {
-            if (_context is not null)
-            {
-                for (int i = 0; i < _context.ForumThreads.Count(); i++)
-                {
-                    return View(i);
-                }
-            }
-            return View();
-        }*/
-
-
-        public IActionResult UpdateThread(ForumThread forumThread)
-        {
-                _context.ForumThreads.Update(forumThread);
-                _context.SaveChanges();
-
-            return View();
-        }
-
-        // Vet ej hur detta ska lösas just nu. TheradId är numer en string / J
-
-        //public IActionResult DeleteThread(int id)
-        //{
-        //    using (var context = _context)
-        //    {
-        //        var thread = context.ForumThreads.SingleOrDefault(t => t.ThreadId == id);
-        //        if (thread != null)
-        //        {
-        //            context.ForumThreads.Remove(thread);
-        //            context.SaveChanges();
-        //        }
-        //    }
-        //    return View();
-        //}
-
+        [HttpGet]
         public IActionResult MyThreads()
         {
-            return View();
+            string? id = User.Identity?.Name;
+            if (id is null)
+            {
+                return BadRequest("You must be logged in.");
+            }
+            var forumThreads = _context.ForumThreads
+                .Where(ft => ft.ApplicationUserId == id)
+                .ToList();
+            return View(forumThreads);
         }
 
+        [HttpGet]
         public IActionResult Thread(int? id)
         {
-            if (!id.HasValue)
+            if (id is null)
             {
                 return BadRequest("You must pass in a ThreadId.");
             }
-          
+            var model = _context.ForumThreads
+                .Include(t => t.Replies)
+                .ThenInclude(r => r.ApplicationUser)
+                .FirstOrDefault(t => t.ThreadId == id);
+            if (model == null)
+            {
+                return NotFound($"Forum thread with ID {id} was not found.");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddReply(int threadId, string replyContent, string threadTitle, string threadContent, int categoryId)
+        {
+            ForumThread? model = _context.ForumThreads
+                .SingleOrDefault(t => t.ThreadId == threadId);
+            List<ForumReply> forumReplies = await _context.ForumReplies.ToListAsync();
+            var reply = new ForumReply
+            {
+                ThreadId = threadId,
+                ApplicationUserId = "USER1-STATIC-ID",
+                PublishDate = DateTime.Now,
+                Content = replyContent,
+                Thread = model
+            };
+
+            _context.ForumReplies.Add(reply);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Thread", new { threadId = model.ThreadId });
+        }
+
+        [HttpGet]
+        public IActionResult UpdateThread(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest("You must pass in a ThreadId.");
+            }
+            var model = _context.ForumThreads
+                .Include(t => t.Replies)
+                .ThenInclude(r => r.ApplicationUser)
+                .FirstOrDefault(t => t.ThreadId == id);
+            if (model == null)
+            {
+                return NotFound($"Forum thread with ID {id} was not found.");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateThread(int id, int categoryId, string threadTitle, string threadContent)
+        {
+            var forumThread = await _context.ForumThreads
+                .FirstOrDefaultAsync(t => t.ThreadId == id);
+            if (forumThread == null)
+            {
+                return NotFound("Thread not found.");
+            }
+            forumThread.ThreadTitle = threadTitle;
+            forumThread.ThreadContent = threadContent;
+            forumThread.CategoryId = categoryId;
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Thread", new { id = forumThread.ThreadId });
+        }
+
+        public async Task<IActionResult> DeleteThread(int? id)
+        {
+            var thread = await _context.ForumThreads
+                .Include(t => t.Replies)
+                .FirstOrDefaultAsync(t => t.ThreadId == id);
+            if (thread == null)
+            {
+                return NotFound("Thread not found");
+            }
+            if (thread != null)
+            {
+                _context.ForumReplies.RemoveRange(thread.Replies);
+                _context.ForumThreads.Remove(thread);
+                await _context.SaveChangesAsync();
+            }
             return View();
         }
+
     }
 }
