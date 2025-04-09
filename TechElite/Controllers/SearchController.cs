3e960cb07a7f3a9c9376f476ce01ed9c04836b9f
Rebeccagh <rebecca.gren.hornfeldt.webd24jon@edu.tucsweden.se>
2025-04-09 11:10:57 +0200

    Search function backend

---
+++ TechElite/Controllers/SearchController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechElite.Models;
+
+namespace TechElite.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public async Task<IActionResult> Search(string search)
+        {
+            if (string.IsNullOrEmpty(search))
+            {
+                return BadRequest("Hittade inget sökord.");
+            }
+
+            var departments = await _context.Departments.ToListAsync();
+            var products = await _context.Products.ToListAsync();
+            var reviews = await _context.Reviews.ToListAsync();
+
+            var filteredDepartments =departments
+                .Where(d => d.DepartmentName.Contains(search, StringComparison.OrdinalIgnoreCase)
+                || d.DepartmentDescription.Contains(search, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+            var filteredProducts = products
+                .Where(p => p.ProductName.Contains(search, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+            var filteredReviews = reviews
+                .Where(r => r.ReviewTitle.Contains(search, StringComparison.OrdinalIgnoreCase)
+                || r.ReviewText.Contains(search, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            var searchViewModel = new SearchViewModel(
+                Departments: filteredDepartments,
+                Products: filteredProducts,
+                Reviews: filteredReviews
+                );
+
+            return View(searchViewModel);
+        }
+    }
+}
