6fac907c164b6101fc2e54000c0a10e8317d17f3
San Zangana <san9@live.se>
2025-04-14 09:55:51 +0200

    Update branch

--- TechElite/Program.cs
+++ TechElite/Program.cs
@@ -35,6 +35,8 @@ builder.Services.AddAuthentication(options =>
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+
 
 
 var app = builder.Build();
@@ -56,6 +58,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseSession();
+
 
 app.UseAuthorization();
 
